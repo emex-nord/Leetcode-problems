a6dd2ac8dacb92e40eb68790a559eb12406bcf7c
emex-nord <128908090+emex-nord@users.noreply.github.com>
2023-08-10 01:57:39 +0300

    Time: 37 ms (90.50%), Space: 16.4 MB (92.27%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -8,23 +8,24 @@ class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
         dummy_node = ListNode()
         dummy_node.next = head
-        current_node = dummy_node
-        for i in range(0, right):
-            if i == left - 1 :
-                before_left = current_node
-                left_node = before_left.next
-            current_node = current_node.next
-        right_node = current_node
-        right_next = right_node.next
+        beforLeft = dummy_node
         
-        prev_node = right_next
-        while left_node.next != right_next:
-            next_node = left_node.next
-            left_node.next = prev_node
-            prev_node = left_node
-            left_node = next_node
-        before_left.next = left_node
-        left_node.next = prev_node
+        for i in range(left - 1):
+            beforLeft = beforLeft.next
+        leftNode = beforLeft.next
+        
+        prevNode = None
+        firstleft = leftNode
+        for k in range(right - left + 1):
+            nextNode = leftNode.next
+            leftNode.next = prevNode
+            prevNode = leftNode
+            leftNode = nextNode
+            
+        beforLeft.next = prevNode
+        firstleft.next = leftNode
+        
         return dummy_node.next
-    
+        
+            
         
