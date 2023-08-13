22b245102205df85d39e122ffa8b4eb70450bdee
emex-nord <128908090+emex-nord@users.noreply.github.com>
2023-08-13 23:36:57 +0300

    Time: 45 ms (61.85%), Space: 16.3 MB (62.57%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -1,5 +1,10 @@
 class Solution:
     def addDigits(self, num: int) -> int:
-        if num == 0:
-            return 0
-        return 1 + (num - 1) % 9
+
+        if len(str(num)) == 1:
+            return num
+        totalSum = 0
+        for i in str(num):
+            totalSum += int(i)
+            
+        return self.addDigits(totalSum)
